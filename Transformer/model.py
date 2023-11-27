e7be703600df34b7f44d0e08f7736c0e3c1121da
petron23 <matipeti@gmail.com>
2023-11-27 13:59:57 +0000

    encoding layers

--- Transformer/model.py
+++ Transformer/model.py
@@ -21,6 +21,25 @@ class PositionalEncoding(nn.Module):
         self.dropout = nn.Dropout(dropout)
 
         # Matrix of shape (seq_len, d_model)
-        pe = torch.zeros(seq_len, d_model)
+        self.pe = torch.zeros(seq_len, d_model)
 
-        # Vector of shape (seq_len, 1)..
+        # Vector of shape (seq_len, 1)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(torch.arange(0,d_model,2).float()) / d_model * (-math.log(10000.)))
+        #div_term = torch.exp((torch.log(10000.**(-torch.arange(0,64,2).float() / 64))))
+
+        # Giving values to the positional encoding matrix
+        self.pe[:, 0::2] = torch.sin(position * div_term)
+        self.pe[:, 1::2] = torch.cos(position * div_term)
+
+
+        # Adding another dimension to the pe matrix as we will use batches
+        self.pe = self.pe.unsqueeze(0) #(1, seq_len, d_model)
+
+        # Register the tensor in the buffer of the model, we want this to be saved not as a learned param
+        # I.e., it is saved in the file along with the model
+        self.register_buffer("pe", self.pe)
+
+    def forward(self, x):
+        x = x + (self.pe[:,:x.shape[1],:]).requires_grad_(False)
+        return self.dropout(x)
