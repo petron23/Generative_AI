a65128199874d716d48b168f67abc29f143940f5
Pit <None>
2023-11-27 12:45:39 +0000

    model

---
+++ Transformer/model.py
@@ -0,0 +1,27 @@
+import torch 
+import torch.nn as  nn
+import math
+
+class InputEmbeddings(nn.Module):
+    def __init__(self, d_model: int, vocab_size: int):
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
+        self.embedding(x) * math.sqrt(self.d_model)
+
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+        # Matrix of shape (seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model)
+
+        # Vector of shape (seq_len, 1).
