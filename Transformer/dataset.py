3772dfe4d57767d677b52a73d98fd6131c4086eb
petron23 <matipeti@gmail.com>
2023-12-06 21:52:00 +0000

    getitem in progress

--- Transformer/dataset.py
+++ Transformer/dataset.py
@@ -5,7 +5,7 @@ from torch.utils.data import Dataset
 
 
 class BilingualDataset(Dataset):
-
+ 
     def __init__(self, ds, tokenizer_src, tokenizer_tgt, src_lang, tgt_lang, seq_len) -> None:
         super().__init__()
 
@@ -27,4 +27,30 @@ class BilingualDataset(Dataset):
         src_text = src_target_pair['translation'][self.src_lang]
         tgt_text = src_target_pair['translation'][self.tgt_lang]
 
+        enc_input_tokens = self.tokenizer_sr.encode(src_text).ids
+        dec_input_tokens = self.tokenizer_sr.decode(tgt_text).ids
+
+        enc_num_padding_tokens = self.seq_len - len(enc_input_tokens) - 2
+        dec_num_padding_tokens = self.seq_len - len(dec_input_tokens) - 1
         
+        #padding token never should become negative
+        if enc_num_padding_tokens < 0 or dec_num_padding_tokens < 0:
+            raise ValueError("Sentence too long")
+        
+        # Start Of Sentence + input tokens + End Of Sentence 
+        encoder_input = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(enc_input_tokens, dtype=torch.int64),
+                self.eos_token,
+                torch.tensor([self.pad_token] * enc_num_padding_tokens, dtype=torch.int64)
+            ]
+        )
+
+        decoder_input = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(dec_input_tokens, dtype=torch.int64),
+                torch.tensor([self.pad_token] * dec_num_padding_tokens, dtype=torch.int64)
+            ]
+         )
