964d922e2af40f4799afce8198fe4798d8bb3d5b
petron23 <matipeti@gmail.com>
2023-12-10 22:07:28 +0000

    dataset finished

--- Transformer/dataset.py
+++ Transformer/dataset.py
@@ -66,3 +66,18 @@ class BilingualDataset(Dataset):
         assert encoder_input.size(0) == self.seq_len
         assert decoder_input.size(0) == self.seq_len
         assert label.size(0) == self.seq_len
+
+        return{
+            "encoder_input": encoder_input, # seq_len
+            "decoder_input": encoder_input, # seq_len
+            "encoder_mask": (encoder_input != self.pad_token).unsqueeze(0).unsqueeze(0).int(), # (1,1, seq_len)
+            "decoder_mask": (decoder_input != self.pad_token).unsqueeze(0).unsqueeze(0).int() & causal_mask(decoder_input.size(0)), # (1, seq_len) & (1, seq_len, seq_len)
+            "label": label, #seq_len
+            "src_text": src_text,
+            "tgt_text": tgt_text
+        }
+
+
+def causal_mask(size):
+    mask = torch.triu(torch.ones(1, size, size), diagonal =1).type(torch.int)
+    return mask == 0
